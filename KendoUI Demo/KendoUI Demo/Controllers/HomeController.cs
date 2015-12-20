7de2dace4c9dbd8d4f5e8a594e7cdfecb71e71b0
Sanjay <sanjaykrishnan1994@gmail.com>
2015-12-20 18:19:40 +0530

    Guys Try this Kendo UI(Open Source You can also add any other Modules)

---
+++ KendoUI Demo/KendoUI Demo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo_UI_Demo_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Kendo_Grid()
+        {
+            return View();
+        }
+
+        public ActionResult MVVM()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ KendoUI Demo/KendoUI Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KendoUI_Demo.Startup))]
+namespace KendoUI_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
